8e320987b2bcc898f8eb64996108dba7c224c5ae
Davi César Batista Monteiro <davi.batista09@aluno.ifce.edu.br>
2025-11-04 12:03:11 +0000

    atualizado

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException 
 from http import HTTPStatus
 from schema import Receita, BaseReceita
 from typing import List
@@ -24,50 +24,51 @@ receitas: List[Receita] = [
     Receita(id=6, nome="Pão de Queijo", ingredientes=["polvilho doce", "queijo minas", "leite", "óleo", "ovos", "sal"], modo_de_preparo="Misture os ingredientes, faça bolinhas e asse.")
 ]
 
-@app.get("/receitas", status_code=HTTPStatus.OK)
+@app.get("/receitas", response_model=List[Receita], status_code=HTTPStatus.OK)
 async def get_receitas():
     return receitas
 
-@app.get("/receitas/{receita_id}")
+@app.get("/receitas/{receita_id}", response_model=Receita, status_code=HTTPStatus.OK)
 async def get_receita_by_id(receita_id: int):
     for receita in receitas:
         if receita.id == receita_id:
             return receita
-    return {"menssagem": "Receita não encontra"}
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
 
-@app.get("/receitas/nome/{receita_nome}")
+@app.get("/receitas/nome/{receita_nome}", response_model=Receita, status_code=HTTPStatus.OK)
 async def get_receita_by_name(receita_nome: str):
     for receita in receitas:
         if receita.nome.lower() == receita_nome.lower():
             return receita
-    return {"menssagem": "Receita não encontrada"}
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
 
 
-
-@app.post("/receitas", status_code=HTTPStatus.CREATED)
+@app.post("/receitas", response_model=Receita, status_code=HTTPStatus.CREATED)
 async def create_receita(receita: BaseReceita):
     for r in receitas:
         if r.nome.lower() == receita.nome.lower():
-            return {"menssagem": "Receita com este nome já existe"}
+            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Receita com este nome já existe") 
 
     if not (2 <= len(receita.nome) <= 50):
-        return {"message": "O nome da receita deve ter entre 2 e 50 caracteres"}
+       raise HTTPException (status_code=HTTPStatus.BAD_REQUEST, detail= "O nome da receita deve ter entre 2 e 50 caracteres")
 
     if not (1 <= len(receita.ingredientes) <= 20):
-        return {"message": "A receita deve ter entre 1 e 20 ingredientes"}
-
-    if len(receitas) > 0:
-        receita.id = receitas[-1].id + 1
-    else:
-        receita.id = 1
+       raise HTTPException (status_code=HTTPStatus.BAD_REQUEST, detail= "A receita deve ter entre 1 e 20 ingredientes")
     
-    receitas.append(receita)
-    return receita
+    if len(receitas) > 0:
+        novo_id = receitas[-1].id + 1
+    else:
+        novo_id=1
+    nova_receita = Receita(
+        id=novo_id,
+        nome=receita.nome,
+        ingredientes = receita.ingredientes,
+        modo_de_preparo = receita.modo_de_preparo
+    )
+    receitas.append(nova_receita)
+    return nova_receita
 
-
-
-
-@app.put("/receitas/{receita_id}", status_code=HTTPStatus.OK)
+@app.put("/receitas/{receita_id}",response_model=Receita, status_code=HTTPStatus.OK)
 async def update_receita(receita_id: int, receita: BaseReceita):
     found_index = -1
     for i, r in enumerate(receitas):
@@ -76,22 +77,22 @@ async def update_receita(receita_id: int, receita: BaseReceita):
             break
 
     if found_index == -1:
-        return {"message": "Receita não encontrada"}
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail= "Receita não encontrada")
 
     for i, r in enumerate(receitas):
         if i != found_index and r.nome.lower() == receita.nome.lower():
-            return {"message": "Já existe uma receita com este nome"}
+            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Receita com este nome já existe")
 
     if not receita.nome or not receita.ingredientes or not receita.modo_de_preparo:
-        return {"message": "Nenhum campo pode ser vazio"}
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Nenhum campo pode ser vazio")
     if any(not ing for ing in receita.ingredientes):
-        return {"message": "Nenhum ingrediente pode ser vazio"}
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Nenhum ingrediente pode ser vazio")
 
     if not (2 <= len(receita.nome) <= 50):
-        return {"message": "O nome da receita deve ter entre 2 e 50 caracteres"}
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="O nome da receita deve ter entre 2 e 50 caracteres")
 
     if not (1 <= len(receita.ingredientes) <= 20):
-        return {"message": "A receita deve ter entre 1 e 20 ingredientes"}
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="A receita deve ter entre 1 e 20 ingredientes")
 
     receita.id = receita_id 
     receitas[found_index] = receita
@@ -99,11 +100,10 @@ async def update_receita(receita_id: int, receita: BaseReceita):
 
 
 
-@app.delete("/receitas/{receita_id}", status_code=HTTPStatus.OK)
+@app.delete("/receitas/{receita_id}",response_model=Receita, status_code=HTTPStatus.OK)
 async def delete_receita(receita_id: int):
     if not receitas:
-        return {"message": "Não há receitas para excluir."}
-
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Não há receitas para excluir.")
     found_index = -1
     deleted_receita = None
     for i, r in enumerate(receitas):
@@ -113,8 +113,9 @@ async def delete_receita(receita_id: int):
             break
 
     if found_index == -1:
-        return {"message": "Receita não encontrada"}
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
 
     receitas.pop(found_index)
     return {"message": f"Receita '{deleted_receita.nome}' (ID: {deleted_receita.id}) foi deletada com sucesso."}
+
 
