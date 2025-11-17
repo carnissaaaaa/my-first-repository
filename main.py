dd1cc0ba9ed073781512c9445c824d5f8e12a6fb
Davi César Batista Monteiro <davi.batista09@aluno.ifce.edu.br>
2025-11-17 13:11:12 +0000

    atualizações de usuários

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from typing import List
 
 app = FastAPI()
 
-usuarios: List[Usuario] = []
-
-receitas: List[Receita] = []
-
 @app.get("/")
 async def read_root():
     return {"message": "Hello World"}
@@ -17,7 +13,7 @@ async def read_root():
 async def read_aluno(nome_aluno: str):
     return {"aluno": nome_aluno}
 
-
+usuarios: List[Usuario] = []
 
 receitas: List[Receita] = [
     Receita(id=1, nome="Bolo de Chocolate", ingredientes=["farinha", "açúcar", "chocolate em pó", "ovos", "leite", "óleo"], modo_de_preparo="Misture tudo e asse."),
@@ -124,15 +120,86 @@ async def delete_receita(receita_id: int):
 
 @app.post("/usuarios", response_model=UsuarioPublic, status_code=HTTPStatus.CREATED)
 async def create_usuario(dados: BaseUsuario):
-    for r in receitas:
-        if r.email == usuarios.email:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Já existe um usuário cadastrado com este email."
-            )
+    for u in usuarios:
+        if u.email == dados.email:
+            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Já existe um usuário cadastrado com este email.")
             
-@app.get("/usuarios", status_code=HTTPException.OK, response_model=List[UsuarioPublic])
-def get_todos_usuarios():
+    if len(usuarios) > 0:
+        novo_id = usuarios[-1].id + 1
+    else:
+        novo_id = 1
+        
+    novo_usuario = Usuario(
+        id=novo_id,
+        nome_usuario=dados.nome_usuario,
+        email=dados.email,
+        senha=dados.senha
+    )
     
-@app.get("/usuarios/{nome_usuario}", )
+    usuarios.append(novo_usuario)
+    return novo_usuario
 
+@app.get("/usuarios", status_code=HTTPStatus.OK, response_model=List[UsuarioPublic])
+async def get_todos_usuarios():
+    return usuarios
+    
+@app.get("/usuarios/{nome_usuario}", response_model=UsuarioPublic, status_code=HTTPStatus.OK)
+async def get_usuario_por_nome(nome_usuario: str):
+    for usuario in usuarios:
+        if usuario.nome_usuario.lower() == nome_usuario.lower():
+            return usuario
+            
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Usuário não encontrado")
+
+@app.get("/usuarios/id/{id}", response_model=UsuarioPublic, status_code=HTTPStatus.OK)
+async def get_usuario_por_id(id: int):
+    for usuario in usuarios:
+        if usuario.id == id:
+            return usuario
+            
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Usuário não encontrado")
+
+@app.put("/usuarios/{id}", response_model=UsuarioPublic, status_code=HTTPStatus.OK)
+async def update_usuario(id: int, dados: BaseUsuario):
+    found_index = -1
+    for i, u in enumerate(usuarios):
+        if u.id == id:
+            found_index = i
+            break
+
+    if found_index == -1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Usuário não encontrado")
+
+    for i, u in enumerate(usuarios):
+        if i != found_index and u.email == dados.email:
+             raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Já existe um usuário com este email")
+
+    usuario_atualizado = Usuario(
+        id=id,
+        nome_usuario=dados.nome_usuario,
+        email=dados.email,
+        senha=dados.senha
+    )
+    
+    usuarios[found_index] = usuario_atualizado
+    return usuario_atualizado
+
+@app.delete("/usuarios/{id}", response_model=UsuarioPublic, status_code=HTTPStatus.OK)
+async def delete_usuario(id: int):
+    if not usuarios:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Não há usuários para excluir.")
+
+    found_index = -1
+    deleted_usuario = None
+    
+    for i, u in enumerate(usuarios):
+        if u.id == id:
+            found_index = i
+            deleted_usuario = u
+            break
+
+    if found_index == -1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Usuário não encontrado")
+
+    usuarios.pop(found_index)
+    return deleted_usuario
--- schema.py
+++ schema.py
@@ -8,18 +8,21 @@ class BaseReceita(BaseModel):
 
 class Receita(BaseReceita):
     id: int 
-                    
+    
+    '''modelo para armazenar os dados do usuário'''       
 class Usuario(BaseModel):
     id: int
     nome_usuario: str
     email: str
     senha: str
-
+    
+    '''modelo para o usuário preencher'''
 class BaseUsuario(BaseModel):
     nome_usuario: str
     email: str
     senha: str
 
+    '''modelo que irá aparecer na página'''
 class UsuarioPublic(BaseModel):
     id: int
     nome_usuario: str
